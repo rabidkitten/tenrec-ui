ffdd4ce72f4a5da4e23606ff281cffbf8cc3fda4
Andrew McKendrick <andrewmckendrick@Andrews-MacBook-Pro.local>
2020-11-30 17:05:15 -0500

    Fixes build and updates doc image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import FontAwesome from './font-awesome';
 import CopyrightYear from './copyright-year/copyright-year';
 import BoxTail from './box-tail/box-tail';
 import Bullseye from './bullseye/bullseye';
+import EmbossedHeading from './embossed-heading/embossed-heading';
 import EstimatedReadTime from './estimated-read-time/estimated-read-time';
 import ZoomImage from './zoom-image/zoom-image';
 
@@ -12,5 +13,6 @@ FontAwesome();
 export { BoxTail };
 export { Bullseye };
 export { CopyrightYear };
+export { EmbossedHeading };
 export { EstimatedReadTime };
 export { ZoomImage };
